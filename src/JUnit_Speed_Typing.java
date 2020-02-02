c0b5a34164215033d7ef1b05c5ea175c04423e37
Jatudrei <yehoshua.kahan@gmail.com>
2020-02-02 19:20:50 +0200

    Minor refactoring

--- src/JUnit_Speed_Typing.java
+++ src/JUnit_Speed_Typing.java
@@ -34,23 +34,19 @@ class JUnit_Speed_Typing {
 		WebElement timer = driver.findElement(By.id("timer"));
 		String time = timer.getText();
 		boolean timeLeft = true;
+		
 		while (timeLeft) {
 			for (int i = 0; i < 4; i++) {
-				String xpath = "//div[@id ='blockLine" + i + "']";//span";
-//				List<WebElement> spans = driver.findElements(By.xpath(xpath));
+				// get the text
+				String xpath = "//div[@id ='blockLine" + i + "']";
 				WebElement div = driver.findElement(By.xpath(xpath));
 				StringBuilder sb = new StringBuilder();
-//				String text = "";
-//				for (int inner = 0; inner < spans.size(); inner++) {
-//					sb.append(spans.get(inner).getText());
-////					text += spans.get(inner).getText();
-//				}
 				sb.append(div.getText());
-//				String text = sb.toString();
+				
+				// push the text
 				inputsDiv.sendKeys(sb.toString());
-//				System.out.println("\"" + sb.toString() + "\"");
-//				System.out.println("===========");
 			}
+			// checking if time's up
 			time = timer.getText();
 			timeLeft = !time.contains("00:00");
 		}
